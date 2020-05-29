6c0c9153d6da231b2a13ccdb15ab190173380750
manzijes <64071693+manzijes@users.noreply.github.com>
2020-05-29 10:53:52 +0200

    Aufgabe 5 und 6

---
+++ Aufgabe5/script.ts
@@ -0,0 +1,70 @@
+
+// tslint:disable-next-line: variable-name
+var Europa: number = 4965.7 + 4616.4 + 4705.8 + 4615.2 + 4541.5 + 4436.9 + 4202 + 4191.6 + 4247.3 + 4278.7 + 4209.3;
+// tslint:disable-next-line: variable-name
+var NorthAmerica: number = 6600.4 + 6139.9 + 6375.8 + 6262.8 + 5998.9 + 6176 + 6268.9 + 6048.7 + 5938.3 + 5920.5 + 6035.6;
+// tslint:disable-next-line: variable-name
+var SouthAmerica: number = 1132.6 + 1093.1 + 1164.2 + 1215.5 + 1262 + 1308.9 + 1331.3 + 1321.7 + 1294.7 + 1279.2 + 1261.5;
+// tslint:disable-next-line: variable-name
+var Africa: number = 1028 + 1041.9 + 1072.1 + 1073.7 + 1107.1 + 1134.4 + 1167.1 + 1174.5 + 1192.1 + 1207 + 1235.5;
+// tslint:disable-next-line: variable-name
+var Asia: number = 12954.7 + 13246.6 + 13986.8 + 14860.1 + 15308.8 + 15660.2 + 15787.7 + 15877 + 15984 + 16274.3 + 16274.1;
+// tslint:disable-next-line: variable-name
+var Australia: number = 1993 + 1875.7 + 1939.1 + 2035.2 + 2063.2 + 2014.7 + 2027.6 + 1986.8 + 1997.4 + 2001.4 + 2100.5;
+
+
+var euDif: number = 4209.3 - 4965.7;
+var naDif: number = 6035.6 - 6600.4;
+var asDif: number = 16274.1 - 12954.7;
+var saDif: number = 1261.5 - 1132.6;
+var afDif: number = 1235.5 - 1028;
+var auDif: number = 2100.5 - 1993;
+
+// tslint:disable-next-line: variable-name
+var Welt: number = Europa + NorthAmerica + Asia + SouthAmerica + Africa + Australia;
+
+var euWorld: number = (Europa / Welt) * 100;
+var naWorld: number = (NorthAmerica / Welt) * 100;
+var asWorld: number = (Asia / Welt) * 100;
+var saWorld: number = (SouthAmerica / Welt) * 100;
+var afWorld: number = (Africa / Welt) * 100;
+var auWorld: number = (Australia / Welt) * 100;
+
+var euRate: number = (euDif / Europa) * 100;
+var naRate: number = (naDif / NorthAmerica) * 100;
+var asRate: number = (asDif / Asia) * 100;
+var saRate: number = (saDif / SouthAmerica) * 100;
+var afRate: number = (afDif / Africa) * 100;
+var auRate: number = (auDif / Australia) * 100;
+
+
+
+console.log(`Die Emission von Europa (2008-2018) ist: ${Europa} kg CO2`);
+console.log(`Relativ zur Gesamtemission der Welt (2008-2018) verursacht Europa damit ${euWorld} %`);
+console.log(`Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um ${euRate}% verändert`);
+console.log(`2018 im Vergleich zu 2008 sind das ${euDif} kg CO2`);
+
+console.log(`Die Emission von Nordamerika (2008-2018) ist: ${NorthAmerica} kg CO2`);
+console.log(`Relativ zur Gesamtemission der Welt (2008-2018) verursacht Nordamerika damit ${naWorld}%`);
+console.log(`Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um ${naRate}% verändert`);
+console.log(`2018 im Vergleich zu 2008 sind das ${naDif} kg CO2`);
+
+console.log(`Die Emission von Asien (2008-2018) ist: ${Asia} kg CO2`);
+console.log(`Relativ zur Gesamtemission der Welt (2008-2018) verursacht Asien damit ${asWorld}%`);
+console.log(`Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um ${asRate}% verändert`);
+console.log(`2018 im Vergleich zu 2008 sind das ${asDif} kg CO2`);
+
+console.log(`Die Emission von Südamerika (2008-2018) ist: ${SouthAmerica} kg CO2`);
+console.log(`Relativ zur Gesamtemission der Welt (2008-2018) verursacht Südamerika damit ${saWorld}%`);
+console.log(`Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emission um ${saRate}% verändert`);
+console.log(`2018 im Vergleich zu 2008 sind das ${saDif} kg CO2`);
+
+console.log(`Die Emission von Afrika (2008-2018) ist: ${Africa} kg CO2`);
+console.log(`Relativ zur Gesamtemission der Welt (2008-2018) verursacht Afrika damit ${afWorld}%`);
+console.log(`Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um ${afRate}% verändert`);
+console.log(`2018 im Vergleich zu 2008 sind das ${afDif} kg CO2`);
+
+console.log(`Die Emission von Australien (2008-2018) ist: ${Australia} kg CO2`);
+console.log(`Relativ zur Gesamtemission der Welt (2008-2018) verursacht Australien damit ${auWorld}%`);
+console.log(`Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um ${auRate}% verändert`);
+console.log(`2018 im Vergleich zu 2008 sind das ${auDif} kg CO2`);
---
+++ Aufgabe6/script.ts
@@ -0,0 +1,59 @@
+function EuropeFunction() {
+    document.getElementById("titleRegion").innerHTML = "Europe";
+    document.getElementById("headline1").innerHTML = "4209.3";
+    document.getElementById("text1").innerHTML = "Emission absolute of Europe in 2018";
+    document.getElementById("headline2").innerHTML = "13.528%";
+    document.getElementById("headline3").innerHTML = "-15.23%";
+    document.getElementById("headline4").innerHTML = "-756.4";
+}
+
+
+function NoAmFunction() {
+    document.getElementById("titleRegion").innerHTML = "North America";
+    document.getElementById("headline1").innerHTML = "6035.6";
+    document.getElementById("text1").innerHTML = "Emission absolute of North America in 2018";
+    document.getElementById("headline2").innerHTML = "19,397%";
+    document.getElementById("headline3").innerHTML = "-8.56%";
+    document.getElementById("headline4").innerHTML = "-564.8";
+    document.getElementById("chart").setAttribute("style", "height: 19.397%");
+}
+
+function SoAmFunction() {
+    document.getElementById("titleRegion").innerHTML = "South America";
+    document.getElementById("headline1").innerHTML = "1261.5";
+    document.getElementById("text1").innerHTML = "Emission absolute of South America in 2018";
+    document.getElementById("headline2").innerHTML = "4.054%";
+    document.getElementById("headline3").innerHTML = "11.38%";
+    document.getElementById("headline4").innerHTML = "128.9";
+    document.getElementById("chart").setAttribute("style", "height: 4.054%");
+}
+
+function AfricaFunction() {
+    document.getElementById("titleRegion").innerHTML = "Africa";
+    document.getElementById("headline1").innerHTML = "1235.5";
+    document.getElementById("text1").innerHTML = "Emission absolute of Africa in 2018";
+    document.getElementById("headline2").innerHTML = "3,971%";
+    document.getElementById("headline3").innerHTML = "20,18%";
+    document.getElementById("headline4").innerHTML = "207.5";
+    document.getElementById("chart").setAttribute("style", "height: 3.971%");
+}
+
+function AsiaFunction() {
+    document.getElementById("titleRegion").innerHTML = "Asia";
+    document.getElementById("headline1").innerHTML = "16274.1";
+    document.getElementById("text1").innerHTML = "Emission absolute of Asia in 2018";
+    document.getElementById("headline2").innerHTML = "52.301%";
+    document.getElementById("headline3").innerHTML = "25.62%";
+    document.getElementById("headline4").innerHTML = "3319.4";
+    document.getElementById("chart").setAttribute("style", "height: 52.301%");
+}
+
+function AustraliaFunction() {
+    document.getElementById("titleRegion").innerHTML = "Australia";
+    document.getElementById("headline1").innerHTML = "2100.5";
+    document.getElementById("text1").innerHTML = "Emission absolute of Australia in 2018";
+    document.getElementById("headline2").innerHTML = " 6.75%";
+    document.getElementById("headline3").innerHTML = "5.39%";
+    document.getElementById("headline4").innerHTML = "107.5";
+    document.getElementById("chart").setAttribute("style", "height: 6.75%");
+}
