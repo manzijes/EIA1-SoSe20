5223da2d18088275c5c4d2bd3982690bdd539a0b
manzijes <64071693+manzijes@users.noreply.github.com>
2020-07-08 13:11:15 +0200

    Aufgabe 11 und steckbrief update

--- Aufgabe10/js/script10.ts
+++ Aufgabe10/js/script10.ts
@@ -401,3 +401,45 @@ function deleteTodo(index: number): void {
     drawListToDOM();
 
 }
+
+//Von hier aus geht es zur Hölle
+
+declare var Artyom: any;
+
+window.addEventListener("load", function(): void {
+    const artyom: any = new Artyom();
+    
+    artyom.addCommands({
+        indexes: ["erstelle Aufgabe *"],
+        smart: true,
+        action: function(i: any, wildcard: string): void {
+            console.log("Neue Aufgabe wird erstellt: " + wildcard);
+            toDoList.unshift(
+                { text: wildcard,
+                checked: false
+                }
+            );
+            drawListToDOM();
+        }
+    });
+    
+    function startContinuousArtyom(): void {
+        artyom.fatality();
+    
+        setTimeout(
+            function(): void {
+                artyom.initialize({
+                    lang: "de-DE",
+                    continuous: true,
+                    listen: true,
+                    interimResults: true,
+                    debug: true
+                }).then(function(): void {
+                    console.log("Ready!");
+                });
+            }, 
+            250);
+    }
+    
+    startContinuousArtyom();
+});
